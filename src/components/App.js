a889603b253528587b7bc6db33da236091d86f49
Sutanu Sain <sutanu.sain@gmail.com>
2021-05-31 02:26:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,10 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 className = "hello" >JavaTpoint</h1>  
+     
             <p data-ns-test = "project-name">This website contains the best CS tutorials.</p> 
+    <p data-ns-test = "project-name">I am learning React. My life is getting better.</p> 
+             
 
 </div>
   )
